4cf42522e01ff4f18d2f5c432799b4f319dd8797
Sean <seanpatmeade@gmail.com>
2023-07-06 20:10:25 +0100

    Base setup of website

---
+++ WEBSITE/rise_and_reflect/rise_and_reflect/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rise_and_reflect project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rise_and_reflect.settings')
+
+application = get_wsgi_application()
