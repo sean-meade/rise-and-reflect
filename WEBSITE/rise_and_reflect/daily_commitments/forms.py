8c7d6acb320c2934d8525569e7a3a30430f2548e
Tomislav Dukez <tomdu3@ymail.com>
2023-07-19 17:39:34 +0100

    fix: fields required according to checkbox in daily commitments

--- WEBSITE/rise_and_reflect/daily_commitments/forms.py
+++ WEBSITE/rise_and_reflect/daily_commitments/forms.py
@@ -4,11 +4,11 @@ from .widget import TimePickerInput
  
 class CommitmentsForm(forms.ModelForm):
     hours_of_sleep = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Hours of Sleep'}))
-    work_time_from = forms.TimeField(widget=TimePickerInput)
-    work_time_to = forms.TimeField(widget=TimePickerInput)
-    commute_time = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Hours to Commute'}))
-    wake_time = forms.TimeField(widget=TimePickerInput)
-    get_ready_time = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Hours to Get Ready for work'}))
+    work_time_from = forms.TimeField(widget=TimePickerInput,required=False)
+    work_time_to = forms.TimeField(widget=TimePickerInput,required=False)
+    commute_time = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Hours to Commute'}),required=False)
+    wake_time = forms.TimeField(widget=TimePickerInput, required=False)
+    get_ready_time = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Hours to Get Ready for work'}),required=False)
 
     class Meta:
         model = UserTimeCommitments
--- WEBSITE/rise_and_reflect/static/js/script.js
+++ WEBSITE/rise_and_reflect/static/js/script.js
@@ -1,11 +1,38 @@
+// array of work form ids
+const workFormArray = [
+  "#id_get_ready_time",
+  "#id_commute_time",
+  "#id_work_time_from",
+  "#id_work_time_to",
+];
+
+// if checkbox ticked, workform appears with all required fields, else wake up with one required field
 $(document).ready(function () {
   $("#toggleForm").on("change", function () {
     if ($(this).is(":checked")) {
-      $("#workForm").slideDown();
+      if ($("#id_wake_time").is(":required")) {
+        $("#id_wake_time").removeAttr("required");
+      }
       $("#wakeUpTimeGroup").slideUp();
+      $("#workForm").slideDown();
+      for (let formElement of workFormArray) {
+        console.log(formElement);
+        if (!$(formElement).is(":required")) {
+          $(formElement).attr("required", "required");
+        }
+      }
     } else {
+      for (let formElement of workFormArray) {
+        console.log(formElement);
+        if ($(formElement).is(":required")) {
+          $(formElement).removeAttr("required");
+        }
+      }
       $("#workForm").slideUp();
       $("#wakeUpTimeGroup").slideDown();
+      if (!$("#id_wake_time").is(":required")) {
+        $("#id_wake_time").attr("required", "required");
+      }
     }
   });
 });
