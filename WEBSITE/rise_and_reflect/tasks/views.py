b2aab637f142204ab8e3014902edc835ebaf28c3
Sean <seanpatmeade@gmail.com>
2023-07-19 01:31:30 +0100

    Create Routine with 8 tasks enetered.

--- WEBSITE/rise_and_reflect/tasks/views.py
+++ WEBSITE/rise_and_reflect/tasks/views.py
@@ -19,13 +19,11 @@ def create_routine(request):
             else:
                 # if the key is just a number on it's own
                 try: 
-                    print(int(key))
                     int(key)
                     # it is an existing task and the user has selected it so add it selected_tasks
                     selected_tasks.append([key,tasks[key + "_time"] ])
                 # If it is a custom one
                 except:
-                    print(key[-4:])
                     if key[:6] == 'custom' and key[-4:] != 'time':
                         # Add the task name and duration to custom_tasks list
                         custom_tasks.append([tasks[key],tasks[key + "_time"] ])
@@ -44,8 +42,36 @@ def create_routine(request):
             # add the id and duration of this newly create task to the selected_tasks list
             selected_tasks.append([this_task.id, duration])
 
-        
-        for task_id, duration_of_task in selected_tasks:
-            routine = UserRoutine(user=request.user, )
+        create_routine_vals = []
+
+        # for x in range(len(selected_tasks)):
+        #     name = "task_id_" + (x + 1)
+        #     value = selected_tasks[x]
+        #     create_routine_vals.append([name, value])
+
+        y = 8 - len(selected_tasks)
+
+        for i in range(y):
+            selected_tasks.append([None, None])
+
+        routine = UserRoutine(user=request.user,
+                              task_id_1=Tasks.objects.get(id=selected_tasks[0][0]),
+                              duration_1=selected_tasks[0][1],
+                              task_id_2=Tasks.objects.get(id=selected_tasks[1][0]),
+                              duration_2=selected_tasks[1][1],
+                              task_id_3=Tasks.objects.get(id=selected_tasks[2][0]),
+                              duration_3=selected_tasks[2][1],
+                              task_id_4=Tasks.objects.get(id=selected_tasks[3][0]),
+                              duration_4=selected_tasks[3][1],
+                              task_id_5=Tasks.objects.get(id=selected_tasks[4][0]),
+                              duration_5=selected_tasks[4][1],
+                              task_id_6=Tasks.objects.get(id=selected_tasks[5][0]),
+                              duration_6=selected_tasks[5][1],
+                              task_id_7=Tasks.objects.get(id=selected_tasks[6][0]),
+                              duration_7=selected_tasks[6][1],
+                              task_id_8=Tasks.objects.get(id=selected_tasks[7][0]),
+                              duration_8=selected_tasks[7][1]
+                              )
+        routine.save()
         
         return redirect(health_goals)
