67380a67a3bd392973dacf72e10feb471cbb5310
pecheylauren02 <pechey.lauren02@gmail.com>
2023-07-20 10:30:53 +0000

    Remove commented out code from urls.py

--- WEBSITE/rise_and_reflect/custom_login/urls.py
+++ WEBSITE/rise_and_reflect/custom_login/urls.py
@@ -7,8 +7,3 @@ urlpatterns = [
     path('daily-commit/', views.daily_commit, name='daily-commit'),
     path('set_goals/', views.set_goals, name='set_goals'),
 ]
-
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
-   # os.path.join(BASE_DIR, 'rise_and_reflect/static'),
-#]
