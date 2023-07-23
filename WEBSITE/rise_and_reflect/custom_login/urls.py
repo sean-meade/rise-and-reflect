a5eccc39110719fb6c59d9eb115582a2e797f898
pjrclarke <pauljrclarke@gmail.com>
2023-07-23 08:07:48 +0100

    removed merge markers

--- WEBSITE/rise_and_reflect/custom_login/urls.py
+++ WEBSITE/rise_and_reflect/custom_login/urls.py
@@ -4,12 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name='home'),
-<<<<<<< HEAD
     path('profile.html', views.profile, name='profile'),
     path('user-homepage.html', views.user_homepage, name='user-homepage'),
-    
-=======
     path('daily-commit/', views.daily_commit, name='daily-commit'),
     path('set_goals/', views.set_goals, name='set_goals'),
->>>>>>> 18cee19a05c8c7f1fc883881d48f62023f5619a1
 ]
