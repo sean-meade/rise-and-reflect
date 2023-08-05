895318de92f7a8bcdb11b17dd9747217461942da
Sean <seanpatmeade@gmail.com>
2023-08-05 22:40:28 +0100

    Add account details and delete account possibility

--- WEBSITE/rise_and_reflect/custom_login/views.py
+++ WEBSITE/rise_and_reflect/custom_login/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from custom_login.models import UserProfile
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import logout as auth_logout, get_user_model
 
 def index(request):
     return render(request, 'home/index.html')
@@ -17,5 +21,14 @@ def set_goals(request):
 
 
 def profile_summary(request):
-    return render(request, 'home/profile_summary.html')
+    user_profile = UserProfile.objects.get(user=request.user)
+    return render(request, 'home/profile_summary.html', {'user_profile': user_profile})
 
+@login_required(login_url='/accounts/login/')
+@require_http_methods(['POST'])
+def delete_account(request):
+    user_pk = request.user.pk
+    auth_logout(request)
+    User = get_user_model()
+    User.objects.filter(pk=user_pk).delete()
+    return render(request, 'home/index.html')
