fcf9b59c7ae92fd269919e51a172a51e92256db1
Sean <seanpatmeade@gmail.com>
2023-08-07 07:39:34 +0100

    add images from cloudinary

--- WEBSITE/rise_and_reflect/rise_and_reflect/urls.py
+++ WEBSITE/rise_and_reflect/rise_and_reflect/urls.py
@@ -1,20 +1,7 @@
-"""rise_and_reflect URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +10,4 @@ urlpatterns = [
     path('', include('daily_commitments.urls')),
     path('tasks/', include('tasks.urls')),
     path('routine/', include('track_routine.urls')),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
