4a99f21e3504777421dc9a45d2973daab8ebafa1
Sean <seanpatmeade@gmail.com>
2024-06-18 11:37:05 +0100

    Display info for Personal Tasks in Admin

--- WEBSITE/rise_and_reflect/tasks/admin.py
+++ WEBSITE/rise_and_reflect/tasks/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Tasks, PersonalTasks, TrackedTasks
 
-admin.site.register(PersonalTasks)
+class PersonalTasksAdmin(admin.ModelAdmin):
+    list_display = ['user', 'task_id', 'duration', 'order']
+    search_fields = ['user', 'task_id', 'duration', 'order']
+
+admin.site.register(PersonalTasks,PersonalTasksAdmin)
 admin.site.register(Tasks)
 admin.site.register(TrackedTasks)
