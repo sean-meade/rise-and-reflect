6312cc5d8aa72f53323570f5afd0ef696828ce87
Sean <seanpatmeade@gmail.com>
2024-06-18 15:01:48 +0100

    Add Tasks info to admin

--- WEBSITE/rise_and_reflect/tasks/admin.py
+++ WEBSITE/rise_and_reflect/tasks/admin.py
@@ -5,6 +5,10 @@ class PersonalTasksAdmin(admin.ModelAdmin):
     list_display = ['user', 'task_id', 'duration', 'order']
     search_fields = ['user', 'task_id', 'duration', 'order']
 
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ['task', 'custom', 'task_type', 'health_area']
+    search_fields = ['task', 'custom', 'task_type', 'health_area']
+
 admin.site.register(PersonalTasks,PersonalTasksAdmin)
-admin.site.register(Tasks)
+admin.site.register(Tasks, TasksAdmin)
 admin.site.register(TrackedTasks)
