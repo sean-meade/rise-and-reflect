055dea7c539401a3c879dc1393d2937f74989dd5
Sean <seanpatmeade@gmail.com>
2024-06-18 12:03:38 +0100

    edit track routine view for new format

--- WEBSITE/rise_and_reflect/track_routine/views.py
+++ WEBSITE/rise_and_reflect/track_routine/views.py
@@ -3,7 +3,7 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import View
-
+from custom_login.models import UserProfile
 from daily_commitments.models import UserTimeCommitments
 from .models import RoutineTasks
 from tasks.models import PersonalTasks, Tasks, TrackedTasks
@@ -14,8 +14,10 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def display_routine(request):
 
+    user = UserProfile.objects.get(user=request.user)
+
     # Get task, type (from Tasks) order,duration, task_id (PersonalTasks)
-    personal_tasks = PersonalTasks.objects.filter(user=request.user).values(
+    personal_tasks = PersonalTasks.objects.filter(user=user).values(
         "task_id", "duration", "order"
     )
 
@@ -41,7 +43,7 @@ def display_routine(request):
             })
 
     # Get hours of sleep
-    list_of_commits = UserTimeCommitments.objects.get(user=request.user).__dict__
+    list_of_commits = UserTimeCommitments.objects.get(user=user).__dict__
     hours_of_sleep = list_of_commits["hours_of_sleep"]
 
     commitments ={}
@@ -137,93 +139,65 @@ def display_routine(request):
 # create new view to handle changing days on page
 @login_required(login_url='/accounts/login/')
 def track_routine(request):
-
-    # get user
-    user = request.user
-    # Get user tasks
-    # get duration and task_id of the users tasks
-    all_user_tasks_tuple = PersonalTasks.objects.filter(user=request.user).values_list(
-        "id", "duration", "task_id"
-    )
-    # turn it to a list of lists
-    all_user_tasks_list = [list(j) for j in all_user_tasks_tuple]
-
-    if request.POST:
-        # list of tasks that are ticked
-        tasks = [int(item) for item in list((request.POST).dict().values()) if item.isdigit()]
-        # List of all tasks of user
-        all_user_tasks_ids = [item[2] for item in all_user_tasks_list ]
-
-        # Run through all the users personal tasks
-        for count, task in enumerate(all_user_tasks_ids):
-            # check to see is that task is ticked
-            if task in tasks:
-                # set that task to true
-                TrackedTasks.objects.filter(personal_task=PersonalTasks.objects.get(task_id=Tasks.objects.get(id=task), user=user)).update(completed=True)
-            else:
-                # Set that task to false
-                TrackedTasks.objects.filter(personal_task=PersonalTasks.objects.get(task_id=Tasks.objects.get(id=all_user_tasks_ids[count]), user=user)).update(completed=False)
-
-    # for every task of the users
-    for task in range(len(all_user_tasks_list)):
-        # Get the task with the id
-        filtered_task = Tasks.objects.get(id=all_user_tasks_list[task][2])
-        # Then add type and task to list
-        all_user_tasks_list[task].append(filtered_task.task_type)
-        all_user_tasks_list[task].append(filtered_task.task_type)
-        all_user_tasks_list[task].append(filtered_task.task)
-        all_user_tasks_list[task].append(filtered_task.custom)
-    # filter RoutineTasks for user to check if a routine exists
-    routine = RoutineTasks.objects.filter(user=user)
+    user = UserProfile.objects.get(user=request.user)
     
-
-    # If a routine exists
-    if routine:
-        try:
-            # check if routine exists for today
-            routine_check = RoutineTasks.objects.filter(day=timezone.now().date(), user=user, routine_type="Morning").first()
-            
-            if routine_check == None:
-                routine_check = RoutineTasks(user=user, routine_type="Morning")
-                routine_check.save()
-
-        # If it is a custom one
-        except:
-            routine_check = RoutineTasks(user=user, routine_type="Morning")
-            routine_check.save()
-
-        # get duration and task_id of the users tasks
-        all_user_tasks_tuple = PersonalTasks.objects.filter(
-            user=request.user
-        ).values_list("duration", "task_id")
-        # turn it to a list of lists
-        all_user_tasks_list = [list(j) for j in all_user_tasks_tuple]
+    tracked_tasks = TrackedTasks.objects.filter(user=user).values_list(
+                                    "personal_task__task_id__task", 
+                                    "personal_task__task_id__task_type",
+                                    "personal_task__duration",
+                                    "completed")
+    
+    if len(tracked_tasks) == 0:
+        all_user_tasks_tuple = PersonalTasks.objects.filter(user=user).values('id', 'task_id', 'task_id__task_type')
         
+        for task in all_user_tasks_tuple:
+            ptask = PersonalTasks.objects.get(id=task['id'])
+            user_routine = RoutineTasks.objects.get(user=user, routine_type=task['task_id__task_type'])
+            tracked_task = TrackedTasks(personal_task=ptask, 
+                                        personal_routine=user_routine, 
+                                        user=user)
+            tracked_task.save()
 
-        # for every task of the users
-        for task in range(len(all_user_tasks_list)):
-            # Get the task using the id
-            filtered_task = Tasks.objects.get(id=all_user_tasks_list[task][1])
-            # Then add type and task to list
-            all_user_tasks_list[task].append(filtered_task.task_type)
-            all_user_tasks_list[task].append(filtered_task.task)
-            all_user_tasks_list[task].append(filtered_task.custom)
-
-            filter_by_this_task = PersonalTasks.objects.get(task_id = filtered_task, user=request.user)
-            try:
-                tracked_task = TrackedTasks.objects.get(personal_task = filter_by_this_task,  personal_routine=routine_check)
-            except:
-                tracked_task = TrackedTasks(personal_task=filter_by_this_task, personal_routine=routine_check, user=request.user)
-                tracked_task.save()
-            
-            all_user_tasks_list[task].append(tracked_task.completed)
-
-        if routine_check:
-            return render(
-                request, "routine/track_routine.html", {"routine": all_user_tasks_list}
-            )
-
+    tracked_tasks = TrackedTasks.objects.filter(user=user).values(
+                                    "id",
+                                    "personal_task__task_id__task", 
+                                    "personal_task__task_id__task_type",
+                                    "personal_task__duration",
+                                    "completed")
     return render(
-        request, "routine/track_routine.html", {"add_tasks": "you need to create tasks and/or go to Create Routine."}
+        request, "routine/track_routine.html", {"routine": list(tracked_tasks)}
     )
 
+def edit_track_routine(request):
+    user = UserProfile.objects.get(user=request.user)
+    
+    if request.POST:
+        tracked_tasks = TrackedTasks.objects.filter(user=user).values(
+                                    "id",
+                                    "personal_task__task_id__task", 
+                                    "personal_task__task_id__task_type",
+                                    "personal_task__duration",
+                                    "completed")
+
+        # get the json from request payload
+        tasks = [int(item) for item in list((request.POST).dict().values()) if item.isdigit()]
+
+        for task in list(tracked_tasks):
+            if task["completed"] == False and task["id"] in tasks:
+                t = TrackedTasks.objects.get(id=task["id"])
+                t.completed = True
+                t.save()
+            else:
+                t = TrackedTasks.objects.get(id=task["id"])
+                t.completed = False
+                t.save()
+    tracked_tasks = TrackedTasks.objects.filter(user=user).values(
+                                    "id",
+                                    "personal_task__task_id__task", 
+                                    "personal_task__task_id__task_type",
+                                    "personal_task__duration",
+                                    "completed")
+    
+    return render(
+        request, "routine/track_routine.html", {"routine": list(tracked_tasks)}
+    )
