dcab2b469db9d71fe71bf551d065824e3a4d5b87
Sean <seanpatmeade@gmail.com>
2024-06-28 11:04:26 +0100

    add more detail to admin pages

--- WEBSITE/rise_and_reflect/custom_login/admin.py
+++ WEBSITE/rise_and_reflect/custom_login/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'logged_in', 'health_area']
+    search_fields = ['user', 'name', 'logged_in', 'health_area']
 
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
--- WEBSITE/rise_and_reflect/track_routine/admin.py
+++ WEBSITE/rise_and_reflect/track_routine/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import RoutineTasks
 
-admin.site.register(RoutineTasks)
+class RoutineTasksAdmin(admin.ModelAdmin):
+    list_display = ['user', 'day', 'routine_type']
+    search_fields = ['user', 'day', 'routine_type']
+
+admin.site.register(RoutineTasks, RoutineTasksAdmin)
