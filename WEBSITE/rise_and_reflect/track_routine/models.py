1a4fe846c4828913e2a7a7e51b3b82d42f8e50ad
Sean <seanpatmeade@gmail.com>
2024-06-28 11:12:07 +0100

    Fix circular import and craete custom delete for RoutineTasks. Check for displaying routine.

--- WEBSITE/rise_and_reflect/track_routine/models.py
+++ WEBSITE/rise_and_reflect/track_routine/models.py
@@ -1,6 +1,6 @@
-from django.conf import settings
 from django.db import models
 from custom_login.models import UserProfile
+from django.apps import apps
 
 
 TASK_TYPE = [
@@ -19,4 +19,23 @@ class RoutineTasks(models.Model):
     def __str__(self):
         # Return a string that represents the instance
         return f"{self.routine_type}"
+    
+    def delete(self, *args, **kwargs):
+        TrackedTasks = apps.get_model('tasks', 'TrackedTasks')
 
+        # Find all tracked tasks associated with this routine
+        tracked_tasks = TrackedTasks.objects.filter(personal_routine=self)
+
+        # For each tracked task, find the personal tasks and delete custom tasks
+        for tracked_task in tracked_tasks:
+            personal_task = tracked_task.personal_task
+
+            # If the task is custom, delete it
+            if personal_task.task_id.custom:
+                personal_task.task_id.delete()
+
+            # Delete the personal task itself
+            personal_task.delete()
+
+        # Delete the routine task
+        super(RoutineTasks, self).delete(*args, **kwargs)
--- WEBSITE/rise_and_reflect/track_routine/views.py
+++ WEBSITE/rise_and_reflect/track_routine/views.py
@@ -1,14 +1,10 @@
 from datetime import datetime, timedelta
-import json
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.views import View
 from custom_login.models import UserProfile
 from daily_commitments.models import UserTimeCommitments
 from .models import RoutineTasks
 from tasks.models import PersonalTasks, Tasks, TrackedTasks
-from django.views.decorators.csrf import csrf_exempt
-from django.utils import timezone
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/accounts/login/')
@@ -16,126 +12,128 @@ def display_routine(request):
 
     user = UserProfile.objects.get(user=request.user)
 
-    # Get task, type (from Tasks) order,duration, task_id (PersonalTasks)
-    personal_tasks = PersonalTasks.objects.filter(user=user).values(
-        "task_id", "duration", "order"
-    )
+    try:
+        # Get task, type (from Tasks) order,duration, task_id (PersonalTasks)
+        personal_tasks = PersonalTasks.objects.filter(user=user).values(
+            "task_id", "duration", "order"
+        )
 
-    morn_tasks = []
+        morn_tasks = []
 
-    eve_tasks = []
+        eve_tasks = []
 
-    for ptask in personal_tasks:
-        t = Tasks.objects.get(id=ptask['task_id'])
-        if t.task_type == "Morning":
-            morn_tasks.append({
-                'task_id': ptask['task_id'],
-                'task': t.task,
-                'duration': ptask['duration'],
-                'order': ptask['order'],
-            })
-        elif t.task_type == "Evening":
-            eve_tasks.append({
-                'task_id': ptask['task_id'],
-                'task': t.task,
-                'duration': ptask['duration'],
-                'order': ptask['order'],
-            })
+        for ptask in personal_tasks:
+            t = Tasks.objects.get(id=ptask['task_id'])
+            if t.task_type == "Morning":
+                morn_tasks.append({
+                    'task_id': ptask['task_id'],
+                    'task': t.task,
+                    'duration': ptask['duration'],
+                    'order': ptask['order'],
+                })
+            elif t.task_type == "Evening":
+                eve_tasks.append({
+                    'task_id': ptask['task_id'],
+                    'task': t.task,
+                    'duration': ptask['duration'],
+                    'order': ptask['order'],
+                })
 
-    # Get hours of sleep
-    list_of_commits = UserTimeCommitments.objects.get(user=user).__dict__
-    hours_of_sleep = list_of_commits["hours_of_sleep"]
+        # Get hours of sleep
+        list_of_commits = UserTimeCommitments.objects.get(user=user).__dict__
+        hours_of_sleep = list_of_commits["hours_of_sleep"]
 
-    commitments ={}
-    commitments['hours_of_sleep'] = hours_of_sleep
+        commitments ={}
+        commitments['hours_of_sleep'] = hours_of_sleep
 
-    # Get either wake time or work commitments
-    wake_time = list_of_commits["wake_time"]
-    if wake_time != None:
-        commitments['wake_time'] = wake_time
-        
-    # for wake time:
-        # Eve
-        # wake time + 5 mins = start time
-        wake_time_fix = datetime.strptime(str(wake_time), '%H:%M:%S')
-        commitments['bed_time'] = (wake_time_fix - timedelta(hours=int(hours_of_sleep))).time()
-
-        morn_start_time = wake_time_fix + timedelta(minutes=5)
-        # start time = time of first task
-        morn_tasks[0]['start_time'] = morn_start_time.time()
-        duration = int(morn_tasks[0]['duration'])
-
-        for morn_task in morn_tasks[1:]:
-       
-            # time of 1st + duration (change to mins) = time of second
-            morn_start_time = morn_start_time + timedelta(minutes=duration)
-            morn_task['start_time'] = morn_start_time.time()
-            duration = int(morn_task['duration'])
+        # Get either wake time or work commitments
+        wake_time = list_of_commits["wake_time"]
+        if wake_time != None:
+            commitments['wake_time'] = wake_time
             
+        # for wake time:
+            # Eve
+            # wake time + 5 mins = start time
+            wake_time_fix = datetime.strptime(str(wake_time), '%H:%M:%S')
+            commitments['bed_time'] = (wake_time_fix - timedelta(hours=int(hours_of_sleep))).time()
+
+            morn_start_time = wake_time_fix + timedelta(minutes=5)
+            # start time = time of first task
+            morn_tasks[0]['start_time'] = morn_start_time.time()
+            duration = int(morn_tasks[0]['duration'])
+
+            for morn_task in morn_tasks[1:]:
+        
+                # time of 1st + duration (change to mins) = time of second
+                morn_start_time = morn_start_time + timedelta(minutes=duration)
+                morn_task['start_time'] = morn_start_time.time()
+                duration = int(morn_task['duration'])
+                
+                # continue
+            # Morn
+            # wake time - sleep hours = bed time
+            eve_start_time = wake_time_fix - timedelta(hours=hours_of_sleep)
+
+            for eve_task in reversed(eve_tasks):
+                duration = int(eve_task['duration'])
+                eve_start_time = eve_start_time - timedelta(minutes=duration)
+                eve_task['start_time'] = eve_start_time.time()
+                        
+        else:
+            
+        # for work commits
+            work_time_from = list_of_commits["work_time_from"]
+            work_time_to = list_of_commits["work_time_to"]
+            commute_time = list_of_commits["commute_time"]
+            get_ready_time = list_of_commits["get_ready_time"]
+
+            # time start work - duration to get ready for work = time to get ready
+            work_time_from_fix = datetime.strptime(str(work_time_from), '%H:%M:%S')
+            get_ready_start = work_time_from_fix - timedelta(minutes=get_ready_time)
+            commitments['get_ready_start'] = get_ready_start.time()
+            commitments['get_ready_time'] = get_ready_time
+            commitments['work_time_from'] = work_time_from
+
+            # Create datetime objects for each time (a and b)
+            dateTimeA = datetime.strptime(str(work_time_to), '%H:%M:%S') 
+            dateTimeB = datetime.strptime(str(work_time_from), '%H:%M:%S') 
+            # Get the difference between datetimes (as timedelta)
+            dateTimeDifference = dateTimeA - dateTimeB
+            # Divide difference in seconds by number of seconds in hour (3600)  
+            dateTimeDifferenceInHours = dateTimeDifference.total_seconds() / 3600
+
+            commitments['work_hours'] = dateTimeDifferenceInHours
+            # time to get ready - duration of last morn task = start time of last morn task
+            morn_start_time = get_ready_start
+            for morn_task in reversed(morn_tasks):
+                duration = int(morn_task['duration'])
+                morn_start_time = morn_start_time - timedelta(minutes=duration)
+                morn_task['start_time'] = morn_start_time.time()
+            # start time of last morn task - duration of second last morn task = start time of second last morn task
+            # continue for all morn tasks
+            # time of first morn task - 15mins = wake time
+            wake_time = morn_start_time - timedelta(minutes=5)
+            commitments['wake_time'] = wake_time.time()
+            # wake time - hours of sleep = bed time
+            bed_time_fix = wake_time - timedelta(hours=int(hours_of_sleep))
+            
+            commitments['bed_time'] = bed_time_fix.time()
+
+            eve_start_time = bed_time_fix
+
+            for eve_task in reversed(eve_tasks):
+                duration = int(eve_task['duration'])
+                eve_start_time = eve_start_time - timedelta(minutes=duration)
+                eve_task['start_time'] = eve_start_time.time()
+            
+            # bed time - duration of last eve task = start time of last eve task
+            # start time of last eve task - duration of second last eve task = start time of second last eve task
             # continue
-        # Morn
-        # wake time - sleep hours = bed time
-        eve_start_time = wake_time_fix - timedelta(hours=hours_of_sleep)
+        return render(request, 'routine/display_routine.html', {'morn_tasks':morn_tasks, 'eve_tasks': eve_tasks, 'commitments': commitments})
+    except:
+        messages.error(request, "You need to create a routine first")
+        return render(request, 'routine/display_routine.html', {'message': 'Please craete Routine in order to view it'})
 
-        for eve_task in reversed(eve_tasks):
-            duration = int(eve_task['duration'])
-            eve_start_time = eve_start_time - timedelta(minutes=duration)
-            eve_task['start_time'] = eve_start_time.time()
-                    
-    else:
-        
-    # for work commits
-        work_time_from = list_of_commits["work_time_from"]
-        work_time_to = list_of_commits["work_time_to"]
-        commute_time = list_of_commits["commute_time"]
-        get_ready_time = list_of_commits["get_ready_time"]
-
-        # time start work - duration to get ready for work = time to get ready
-        work_time_from_fix = datetime.strptime(str(work_time_from), '%H:%M:%S')
-        get_ready_start = work_time_from_fix - timedelta(minutes=get_ready_time)
-        commitments['get_ready_start'] = get_ready_start.time()
-        commitments['get_ready_time'] = get_ready_time
-        commitments['work_time_from'] = work_time_from
-
-        # Create datetime objects for each time (a and b)
-        dateTimeA = datetime.strptime(str(work_time_to), '%H:%M:%S') 
-        dateTimeB = datetime.strptime(str(work_time_from), '%H:%M:%S') 
-        # Get the difference between datetimes (as timedelta)
-        dateTimeDifference = dateTimeA - dateTimeB
-        # Divide difference in seconds by number of seconds in hour (3600)  
-        dateTimeDifferenceInHours = dateTimeDifference.total_seconds() / 3600
-
-        commitments['work_hours'] = dateTimeDifferenceInHours
-        # time to get ready - duration of last morn task = start time of last morn task
-        morn_start_time = get_ready_start
-        for morn_task in reversed(morn_tasks):
-            duration = int(morn_task['duration'])
-            morn_start_time = morn_start_time - timedelta(minutes=duration)
-            morn_task['start_time'] = morn_start_time.time()
-        # start time of last morn task - duration of second last morn task = start time of second last morn task
-        # continue for all morn tasks
-        # time of first morn task - 15mins = wake time
-        wake_time = morn_start_time - timedelta(minutes=5)
-        commitments['wake_time'] = wake_time.time()
-        # wake time - hours of sleep = bed time
-        bed_time_fix = wake_time - timedelta(hours=int(hours_of_sleep))
-        
-        commitments['bed_time'] = bed_time_fix.time()
-
-        eve_start_time = bed_time_fix
-
-        for eve_task in reversed(eve_tasks):
-            duration = int(eve_task['duration'])
-            eve_start_time = eve_start_time - timedelta(minutes=duration)
-            eve_task['start_time'] = eve_start_time.time()
-        
-        # bed time - duration of last eve task = start time of last eve task
-        # start time of last eve task - duration of second last eve task = start time of second last eve task
-        # continue
-    return render(request, 'routine/display_routine.html', {'morn_tasks':morn_tasks, 'eve_tasks': eve_tasks, 'commitments': commitments})
-
-# TODO:
-# Separate evening from morning
 # create new view to handle changing days on page
 @login_required(login_url='/accounts/login/')
 def track_routine(request):
